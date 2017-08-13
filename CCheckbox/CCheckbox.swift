43869744eb7013a0905b608944c3f222d71fee89
Mohamed Salah <mohammed.salah1193@gmail.com>
2017-08-13 14:37:16 +0200

    Fix issue #1 - Add is Checkbox selected in Attributes Inspector - Make normalImage and selectedImage reflects on button in the interface builder

--- CCheckbox/CCheckbox.swift
+++ CCheckbox/CCheckbox.swift
@@ -9,79 +9,53 @@
 import UIKit
 
 /**
-    CheckboxDelegate protocol to be confirmed 
-    when you need callbacks for checkboc status change
+ CheckboxDelegate protocol to be confirmed
+ when you need callbacks for checkboc status change
  **/
-protocol CheckboxDelegate {
+@objc protocol CheckboxDelegate {
     func didSelect(_ checkbox: CCheckbox)
     func didDeselect(_ checkbox: CCheckbox)
 }
 
 /**
- CCheckbox is a custom UIButton class 
+ CCheckbox is a custom UIButton class
  which represents custom checkbox with
  custom images
  **/
 
-@IBDesignable
-class CCheckbox: UIButton {
+@IBDesignable class CCheckbox: UIButton {
+    
     //MARK:- Variables
-    private var checkboxImage: UIImageView!
-    
-    @IBInspectable var normalImage: UIImage? {
-        didSet {
-            checkboxImage.image = normalImage
-        }
-    }
-    
-    @IBInspectable var selectedImage: UIImage?
-    
-    
+    @IBInspectable var normalImage: UIImage? = UIImage()
+    @IBInspectable var selectedImage: UIImage? = UIImage()
+
     /// when set this variable checkbox status changed
-    var isCheckboxSelected: Bool = false {
+    @IBInspectable var isCheckboxSelected: Bool = false {
         didSet {
+            self.imageView?.contentMode = .scaleAspectFit
             self.changeStatus()
         }
     }
-    
-    var delegate: CheckboxDelegate!
+
+    open weak var delegate: CheckboxDelegate?
     var animation: UIViewAnimationOptions = .transitionCrossDissolve
     
-    //MARK:- init Section
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.initComponent()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.initComponent()
+    //MARK:- Customize Button Properties
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        initComponent()
     }
     
     private func initComponent () {
-        
         //Check Button Type Value
         if self.buttonType != .system
             && self.buttonType != .custom {
-            
             fatalError("Button Type Error. Please Make Button Type System or Custom")
         }
-        
-        // build component frame
-        let componentsFrame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-        
-        // adding imageView to the CCheckbox view using its frame
-        checkboxImage = UIImageView(frame: componentsFrame)
-        checkboxImage.image = normalImage
-        checkboxImage.contentMode = .scaleAspectFit
-        
-        self.addSubview(checkboxImage)
-        
-        self.setImage(nil, for: .normal)
         self.setTitle("", for: .normal)
         self.addTarget(self,
-                              action: #selector(CCheckbox.didTouchUpInside(_:)),
-                              for: .touchUpInside)
+                       action: #selector(CCheckbox.didTouchUpInside(_:)),
+                       for: .touchUpInside)
         
         //Styling UIView
         self.backgroundColor = UIColor.clear
@@ -90,33 +64,30 @@ class CCheckbox: UIButton {
     //MARK:- Checkbox Status
     private func changeStatus() {
         if isCheckboxSelected {
-            UIView.transition(with: self.checkboxImage,
-                                      duration:0.5,
-                                      options: animation,
-                                      animations: { self.checkboxImage.image = self.selectedImage },
-                                      completion: nil)
-            
-            
-        } else {
-            UIView.transition(with: self.checkboxImage,
+            UIView.transition(with: self.imageView!,
                               duration:0.5,
                               options: animation,
-                              animations: { self.checkboxImage.image = self.normalImage },
+                              animations: { self.setImage(self.selectedImage, for: .normal) },
+                              completion: nil)
+
+        } else {
+            UIView.transition(with: self.imageView!,
+                              duration:0.5,
+                              options: animation,
+                              animations: { self.setImage(self.normalImage, for: .normal) },
                               completion: nil)
         }
-        
+    
     }
     
     // MARK: IBActions
     func didTouchUpInside(_ sender: UIButton) {
         self.isCheckboxSelected = !self.isCheckboxSelected
         
-        
         if isCheckboxSelected && delegate != nil {
-            delegate.didSelect(self)
+            delegate?.didSelect(self)
         } else if delegate != nil {
-            delegate.didDeselect(self)
+            delegate?.didDeselect(self)
         }
     }
-    
 }
